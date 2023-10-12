224094b8965472b108926765883a329b7accc49e
EgorPetkevich <egorpetkevich01@gmail.com>
2023-10-12 19:51:50 +0300

    first commit

---
+++ CharacterInfo/APIResponse.swift
@@ -0,0 +1,8 @@
+//
+//  APIResponse.swift
+//  CharacterInfo
+//
+//  Created by George Popkich on 12.10.23.
+//
+
+import Foundation
---
+++ CharacterInfo/NetworkService.swift
@@ -0,0 +1,8 @@
+//
+//  NetworkService.swift
+//  CharacterInfo
+//
+//  Created by George Popkich on 12.10.23.
+//
+
+import Foundation
